0cf6e30ca3b7dc0d80166df76c4642eb10f203d4
AC1556 <allenclark@u.boisestate.edu>
2022-06-16 20:05:02 +0900

    filter and result layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <Header />
+  <div id="content" class="flex flex-row gap-8">
+    <aside class="w-1/4">
+      <Card>
+        <p>filters</p>
+        <Button label="Click me!" />
+      </Card>
+    </aside>
+    <main class="w-screen">
+      <Card>
+        <p>main results</p>
+        <Button label="Click me!" />
+      </Card>
+    </main>
   </div>
 </template>
 

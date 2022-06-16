ff47a49e5f307ce58827f9ba9a2d125e4d6d9647
AC1556 <allenclark@u.boisestate.edu>
2022-06-16 20:02:57 +0900

    Sticky header

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <script setup></script>
 
 <template>
-  <div>
-    <Navbar />
-    <NuxtPage />
+  <div class="h-screen">
+    <Header />
+    <div class="w-full top-0 z-50 sticky">
+      <SearchBar />
+    </div>
+
+    <div class="relative ml-28 mr-28">
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,29 @@
-<script lang="ts" setup></script>
+<script setup></script>
 
 <template>
-  <div>
-    <Search />
-  </div>
+  <nav class="flex items-center bg-white-1">
+    <img :alt="$t('alt.logo')" src="~/assets/img/logo.jpg" class="h-8 m-3" />
+
+    <ul class="flex ml-auto gap-5">
+      <li>
+        <select>
+          <option>{{ $t('local.lang') }}</option>
+        </select>
+      </li>
+
+      <li>
+        <select>
+          <option>{{ $t('local.cur.usd') }}</option>
+        </select>
+      </li>
+      <li>
+        <button>{{ $t('nav.account') }}</button>
+      </li>
+      <li>
+        <button>{{ $t('nav.logout') }}</button>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>

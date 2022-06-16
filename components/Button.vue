61e16dad8fb90dcd63710c0e114b5dc2f37b360a
AC1556 <allenclark@u.boisestate.edu>
2022-06-16 22:24:35 +0900

    Using typescript; better prop def

--- components/Button.vue
+++ components/Button.vue
@@ -1,17 +1,18 @@
-<script setup>
-defineProps({
-  label: {
-    type: String,
-    default: '',
-  },
-});
+<script lang="ts" setup>
+defineProps<{
+  isDisabled?: boolean;
+  label?: string;
+}>();
 </script>
 
 <template>
   <button
     @click="$emit('btn-click')"
     class="p-3 bg-blue-2 text-white-1 rounded"
+    :disabled="isDisabled"
   >
-    {{ label }}
+    <h4>
+      {{ label }}
+    </h4>
   </button>
 </template>

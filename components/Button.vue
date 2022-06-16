a8552a82380fe0b637c5be46fb01bfaf40703827
AC1556 <allenclark@u.boisestate.edu>
2022-06-16 20:03:27 +0900

    Card and search component

--- components/Button.vue
+++ components/Button.vue
@@ -8,5 +8,10 @@ defineProps({
 </script>
 
 <template>
-  <button class="p-3 bg-blue-2 text-white-1 rounded">{{ label }}</button>
+  <button
+    @click="$emit('btn-click')"
+    class="p-3 bg-blue-2 text-white-1 rounded"
+  >
+    {{ label }}
+  </button>
 </template>
---
+++ components/Card.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="bg-white-1 rounded p-5 w-full mt-2 mb-2">
+    <slot />
+  </div>
+</template>

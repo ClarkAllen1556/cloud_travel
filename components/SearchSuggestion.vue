9a8ea93b6340be0256934a576bc96899533efe5e
AC1556 <allenclark@u.boisestate.edu>
2022-06-18 18:57:42 +0900

    Suggestion components

---
+++ components/SearchSuggestion.vue
@@ -0,0 +1,18 @@
+<script lang='ts' setup>
+defineProps<{
+  cityName: string
+}>();
+</script>
+
+<template>
+  <div class='flex gap-2 p-2.5 items-center city-entry hover:bg-grey-1 hover:cursor-pointer'>
+    <img src='~/assets/img/location-pin.svg' />
+
+    <p>
+      {{ cityName }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ components/SearchSuggestionBox.vue
@@ -0,0 +1,11 @@
+<script lang='ts' setup>
+defineProps<{
+  query?: string;
+}>();
+</script>
+
+<template>
+  <div class='w-96 absolute bg-white-1 rounded mt-0.5 border border-grey-3'>
+    <slot />
+  </div>
+</template>

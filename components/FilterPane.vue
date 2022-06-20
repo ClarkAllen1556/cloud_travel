2705d5192c202202c4b4fc4738c509a674921dc2
AC1556 <allenclark@u.boisestate.edu>
2022-06-20 14:56:27 +0900

    Abstracting filter pane to `App.vue`

---
+++ components/FilterPane.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <slot />
+
+    <Card>
+      <p> Filters </p>
+
+      <Button label='click me! 🤗' />
+    </Card>
+  </div>
+</template>
+
+<style scoped></style>

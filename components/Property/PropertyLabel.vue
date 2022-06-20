7c0986ca2bbe8586ef474a076fb332ac8103d07d
AC1556 <allenclark@u.boisestate.edu>
2022-06-20 17:03:51 +0900

    Creating property label component

---
+++ components/Property/PropertyLabel.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+
+defineProps<{
+  foodCode: FoodCode
+}>()
+
+</script>
+
+<script lang='ts'>
+type FoodCode = {
+  RoomOnly: {
+    code: 1,
+    label: 'ROOM_ONLY'
+  },
+  Breakfast: {
+    code: 2,
+    label: 'BREAK_FAST'
+  },
+  Lunch: {
+    code: 3,
+    label: 'LUNCH'
+  },
+  Dinner: {
+    code: 4
+    label: 'DINNER'
+  },
+  Half: {
+    code: 5
+    label: 'HALF'
+  },
+  Full: {
+    code: 6
+    label: 'FULL'
+  },
+  All: {
+    code: 7
+    label: 'ALL'
+  }
+}
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped></style>

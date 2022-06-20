2dec6083bf6029601d1e3b5e7085668ee7a96cec
AC1556 <allenclark@u.boisestate.edu>
2022-06-20 14:58:10 +0900

    Using new abstracted `FilterPane` component; filter pane will now be sticky; fixing search scrolling after 100vh scrolled

--- app.vue
+++ app.vue
@@ -1,14 +1,23 @@
-<script lang='ts' setup></script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="h-screen">
+  <div class="h-full">
     <Header />
+
     <div class="w-full top-0 z-50 sticky">
       <SearchBar />
     </div>
 
-    <div class="relative ml-28 mr-28">
-      <NuxtPage />
+    <div class="relative ml-28 mr-28 flex flex-row gap-2">
+      <aside class="w-72 shrink-0 relative">
+        <FilterPane class="sticky mt-4 top-20">
+          <MapCard />
+        </FilterPane>
+      </aside>
+
+      <main class="w-screen">
+        <NuxtPage />
+      </main>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,23 @@
 <script lang="ts" setup>
+// TODO migrate this into a state composable
 const isLoading = useState<boolean>('isLoading', () => false);
-const currentCity = useState('currentCity', () => {});
-const searchResults = useState('searchResults', () => {});
+const currentCity = useState<string>('currentCity', () => "");
+const searchResults = useState<any>('searchResults', () => {});
 </script>
 
 <template>
-  <div id="content" class="flex flex-row gap-4">
-    <aside class="w-72 shrink-0">
-      <MapCard />
-
-      <Card>
-        <p>filters</p>
-        <Button label="Click me!" />
-      </Card>
-    </aside>
-    <main class="w-screen">
+  <div id="content">
       <SortPane />
 
       <Card
           v-for='(location, i) in searchResults?.outlets.availability.results'
           :key='i'
-          class='hover:shadow hover:cursor-pointer'
+          class='hover:shadow'
       >
         <div>
           <Property :location='location' />
         </div>
       </Card>
-    </main>
   </div>
 </template>
 

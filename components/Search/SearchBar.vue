c1707ed80e0ef69d6a35c691721dd5c15af69816
AC1556 <allen@urbanlife.me>
2022-06-22 00:13:35 +0900

    autoformatting

--- components/Search/SearchBar.vue
+++ components/Search/SearchBar.vue
@@ -1,55 +1,65 @@
-<script lang='ts' setup>
-import SearchSuggestionContainer from '~/components/Search/Suggestion/SearchSuggestionContainer.vue';
-
-const { $fetchResource, $Resources } = useNuxtApp()
+<script lang="ts" setup>
+const { $fetchResource, $Resources } = useNuxtApp();
 
 const currentCity = useState('currentCity');
-const searchResults = useState('searchResults')
-const { data: cities } = $fetchResource($Resources.AutoSuggest)
+const searchResults = useState('searchResults');
+const { data: cities } = $fetchResource($Resources.AutoSuggest);
 
 const query = ref<string>('');
 const showSuggestions = ref<boolean>(false);
 
 watch(currentCity, async (nCityCode: string) => {
-  const { data: results } = await $fetchResource($Resources.Search, nCityCode, [ 'outlets' ])
+  const { data: results } = await $fetchResource($Resources.Search, nCityCode, [
+    'outlets',
+  ]);
 
-  searchResults.value = results
-})
+  searchResults.value = results;
+});
 
-function setShowSuggestions (isShow: boolean) {
-  showSuggestions.value = isShow
+function setShowSuggestions(isShow: boolean) {
+  showSuggestions.value = isShow;
 }
 
 function setCurrentCity(cityCode: string) {
-  currentCity.value = cityCode
+  currentCity.value = cityCode;
 }
 
-function handleCitySelection (cityCode) {
-  setCurrentCity(cityCode)
-  setShowSuggestions(false)
+function handleCitySelection(cityCode) {
+  setCurrentCity(cityCode);
+  setShowSuggestions(false);
 }
 </script>
 
 <template>
-  <div class='flex bg-blue-1 p-3 pl-32 pr-32'>
-    <div class='w-96'>
-      <div class='flex border border-grey-3 rounded bg-white-1'>
-        <img src='~/assets/img/search.svg' class='w-4 h-4 m-3' :alt='$t("alt.search_icon")' />
+  <div class="flex bg-blue-1 p-3 pl-32 pr-32">
+    <div class="w-96">
+      <div class="flex border border-grey-3 rounded bg-white-1">
+        <img
+          src="~/assets/img/search.svg"
+          class="w-4 h-4 m-3"
+          :alt="$t('alt.search_icon')"
+        />
 
-        <input class='w-full outline-none' v-model='query' @focus='setShowSuggestions(true)'/>
+        <input
+          class="w-full outline-none"
+          v-model="query"
+          @focus="setShowSuggestions(true)"
+        />
       </div>
 
-      <SearchSuggestionContainer v-if='showSuggestions && query && query.trim().length'>
+      <SearchSuggestionContainer
+        v-if="showSuggestions && query && query.trim().length"
+      >
         <SearchSuggestionItem
-          v-for='(city, i) in cities'
-          :city='city'
-          :key='i'
-          @item-clicked='handleCitySelection'
+          v-for="(city, i) in cities"
+          :city="city"
+          :key="i"
+          @item-clicked="handleCitySelection"
         />
       </SearchSuggestionContainer>
     </div>
 
-    <div class='ml-auto'>
+    <div class="ml-auto">
       <Button :label="$t('action.search')" />
     </div>
   </div>

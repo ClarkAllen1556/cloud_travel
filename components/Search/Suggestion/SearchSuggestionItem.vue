edd889c4c725177d9fd1acb8d4ccaf2cd7dd237e
AC1556 <allen@urbanlife.me>
2022-06-22 15:22:34 +0900

    auto formatting

--- components/Search/Suggestion/SearchSuggestionItem.vue
+++ components/Search/Suggestion/SearchSuggestionItem.vue
@@ -1,27 +1,25 @@
-<script lang='ts' setup>
+<script lang="ts" setup>
 const { city } = defineProps<{
   city: {
-    label: string,
-    cityCode: string
-  }
+    label: string;
+    cityCode: string;
+  };
 }>();
 
 const emit = defineEmits<{
-  (event: 'item-clicked', cityCode: string): void
-}>()
-
+  (event: 'item-clicked', cityCode: string): void;
+}>();
 </script>
 
 <template>
   <div
-    class='flex gap-2 p-2.5 items-center city-entry hover:bg-grey-1 hover:cursor-pointer'
-    @click='emit("item-clicked", city.cityCode)'
+    class="flex gap-2 p-2.5 items-center city-entry hover:bg-grey-1 hover:cursor-pointer"
+    @click="emit('item-clicked', city.cityCode)"
   >
-    <img src='~/assets/img/location-pin.svg' :alt='$t("alt.location_pin")' />
+    <img src="~/assets/img/location-pin.svg" :alt="$t('alt.location_pin')" />
 
-    <p> {{ city.label }} </p>
+    <p>{{ city.label }}</p>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>

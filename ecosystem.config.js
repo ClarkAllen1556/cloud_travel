989ddc05c7782e4e93ef4431744342575ed6117d
AC1556 <allen@urbanlife.me>
2022-06-22 20:19:04 +0900

    pm2 port config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '1',
       script: './.output/server/index.mjs',
+      env: {
+        NITRO_PORT: 3101,
+      },
     },
   ],
 };
